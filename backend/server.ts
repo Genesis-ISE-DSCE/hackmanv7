b8f1bf0c505885b9f9845e634f021032717e3b0a
ManishMadan2882 <manishmadan321@gmail.com>
2024-02-05 15:38:25 +0530

    initiated server listener

--- backend/server.ts
+++ backend/server.ts
@@ -1,5 +1,11 @@
-import { PrismaClient } from "@prisma/client"
+import { PrismaClient } from "@prisma/client";
+import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
 const prisma = new PrismaClient();
+const app = express();
+const port: number = Number(process.env.PORT) || 8000;
+app.use(express.json())
 async function main() {
     //here comes our queries
 }
@@ -11,3 +17,4 @@ main().then(async () => {
         await prisma.$disconnect();
         process.exit(1);
     })
+app.listen(port, () => console.log('Server is running at ' + port))
