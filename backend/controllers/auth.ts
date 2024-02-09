cad62254af91bb0bf1139bd305da646850d6580e
Aditya Agarwal <adi790u@Adityas-MacBook-Air.local>
2024-02-10 00:14:47 +0530

    Error handler, register controller and some schema changes

--- backend/controllers/auth.ts
+++ backend/controllers/auth.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import { db } from "../lib/db";
+import { db } from "../utils/db";
 
 // id Int @id @default(autoincrement())
 // name String
@@ -9,6 +9,45 @@ import { db } from "../lib/db";
 // teamId Int
 // isLead Boolean @default(false)
 
+// leader -->
+
+// name,
+// email,
+
+interface Participant {
+  name: string;
+  email: string;
+  phoneNumber: string;
+  isLead: string;
+}
+
+interface TeamInfo {
+  name: string;
+  upiId: string;
+  transactionId: string;
+}
+
 export const registerController = async (req: Request, res: Response) => {
-  const { team, email, phoneNumber, isLead } = req.body;
+  const { members, teamInfo }: { members: Participant[]; teamInfo: TeamInfo } =
+    req.body;
+
+  const team = await db.team.create({
+    data: {
+      teamName: teamInfo.name,
+      upiId: teamInfo.upiId,
+      transactionId: teamInfo.transactionId,
+    },
+  });
+
+  members.map(async (member) => {
+    const teamMate = await db.participant.create({
+      data: {
+        name: member.name,
+        email: member.email,
+        phoneNumber: member.phoneNumber,
+        isLead: member.isLead === "true",
+        teamId: team.id,
+      },
+    });
+  });
 };
---
+++ backend/prisma/migrations/20240209181807_change/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "Team" ALTER COLUMN "githubLink" DROP NOT NULL;
