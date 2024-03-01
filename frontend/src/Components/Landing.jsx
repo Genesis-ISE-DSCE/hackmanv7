ff17938bd930cb74f6e9dba73bafa22b5ab4a895
Saakshi-Shenoy <shenoy.saakshi910@gmail.com>
2024-03-01 23:49:01 +0530

    new mascot added

--- frontend/src/Components/Landing.jsx
+++ frontend/src/Components/Landing.jsx
@@ -1,8 +1,10 @@
-import React from 'react'
-import newBg from "../assets/new-bg.png"
-import '../App.css'
-import About from './About'
-import mascot from "../assets/mascot.png"
+import React, { useState, useEffect } from 'react';
+import newBg from "../assets/new-bg.png";
+import '../App.css';
+import About from './About';
+import Bat from "../assets/bat-mascot.gif";
+import Dialogue from "../assets/dialogue.png";
+import Dialogue2 from "../assets/dialogue2.png";
 import Typewriter from 'typewriter-effect/dist/core';
 
 // new Typewriter('#typewriter', {
@@ -11,6 +13,24 @@ import Typewriter from 'typewriter-effect/dist/core';
 // });
 
 function Landing() {
+
+  const [showDialogue, setShowDialogue] = useState(true);
+  const [showAnotherDialogue, setShowAnotherDialogue] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowDialogue(false);
+    }, 20000);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, []);
+
+  const handleHover = () => {
+    setShowDialogue(false);
+    setShowAnotherDialogue(true);
+  };
 
   const handleScroll = (event) => {
     const container = event.target;
@@ -40,14 +60,29 @@ function Landing() {
       <div className="overlay"></div>
       <img src={newBg} alt="" srcset=""  style={{height:"120vh",width:"100vw",objectFit:"cover"}}/> 
       </section>
-     {/* <div className="mascot ">
+     <div className="mascot" onMouseOver={handleHover}>
         <picture  alt="mascot" >
-           <img height={250} width={240} src={mascot} loading="lazy" alt="mascot"/>
-        </picture>
-      </div> */}
-      </div>
-      
+           <img height={200} width={210} src={Bat} loading="lazy" alt="mascot"/>
+        </picture> 
 
+        {showDialogue && (
+            <div className="dialogue" id="dialogue">
+              <picture alt="dialogue-box" >
+                <img height={90} width={140} src={Dialogue} loading="lazy" alt="hello" />
+              </picture>
+            </div>
+        )}
+
+        {showAnotherDialogue && (
+            <div className="dialogue2" id="dialogue2">
+              <picture alt="dialogue-box2" >
+                <img height={120} width={200} src={Dialogue2} loading="lazy" alt="hello" />
+              </picture>
+            </div>
+        )}
+
+      </div>
+      </div>
     </div>
   )
 }
