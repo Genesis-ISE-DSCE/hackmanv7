091ecbdee6e3501dd8c5717d6729f4ec60f34c36
Astraxx04 <gagan200254@gmail.com>
2024-04-09 19:37:15 +0530

    Minor updates contact

--- frontend/src/Components/Footer.jsx
+++ frontend/src/Components/Footer.jsx
@@ -1,19 +1,74 @@
 import React from "react";
-import Board from "../assets/board-contact.png";
 import { Linkedin, Instagram, Mail } from "react-feather";
 import "../Css/footer.css";
+import img from "../assets/images/WHAT__1_-removebg-preview.png";
 
 function Footer() {
   return (
     <div className="about-con custom-foot-bg" id="contact">
-      <div className="abt-container">
-        <div className="text-center">
-        <h1 className="text-center mt-0">C O N T A C T || U S</h1>
-          <div
-            data-aos="flip-down"
-            className="content-box small-text container-md container-sm transform transition-transform hover:scale-120"
-          >
-            <div>
+      <div className="text-center">
+        <h1 className="text-center title-sub">C O N T A C T || U S</h1>
+          <div data-aos="flip-down" className="content-box">
+            <div className="reachout-con">
+              <h2 className="connectHeading">
+                Reach out to us
+              </h2>
+              <div className="num-link">
+                <p>Gagan : <a className="doc-link" href="">+91 9008243280</a></p>
+                <p>Tarana : <a className="doc-link" href="">+91 9008243280</a></p>
+              </div>
+            </div>
+            <h2 className="connectHeading">
+              Connect with us
+            </h2>
+            <div className="links-box">
+              <div className="social-links">
+                <a
+                  href="https://www.instagram.com/wearehackman/"
+                  rel="noreferrer noopener"
+                  target="_blank"
+                >
+                  <Instagram size={50} />
+                </a>
+              </div>
+
+              <div className="social-links">
+                <a
+                  href="https://www.linkedin.com/company/wearehackman/"
+                  rel="noreferrer noopener"
+                  target="_blank"
+                >
+                  <Linkedin size={50} />
+                </a>
+              </div>
+
+              <div className="social-links">
+                <a
+                  href="mailto:genesis.hackman@gmail.com"
+                  rel="noreferrer noopener"
+                  target="_blank"
+                >
+                  <Mail size={50} />
+                </a>
+              </div>
+            </div>
+            <div className="documents">
+              <p className="doc-link"><a href="">Code of Conduct</a></p>
+              <p className="doc-link"><a href="">Terms and Conditions</a></p>
+              <p className="doc-link"><a href="">Instructions</a></p>
+            </div>
+          </div>
+        </div>
+      <div className="madeby">Made by team <a href="https://www.instagram.com/genesis.ise/" rel='noreferrer noopener' target="_blank" className='genesis-link'>Genesis</a> with <span className="heart">❤️</span></div>
+    </div>
+  );
+}
+
+export default Footer;
+
+
+
+{/* <div>
               <h2
                 style={{
                   fontFamily: "pixeloidsans-bold",
@@ -29,81 +84,10 @@ function Footer() {
               className="small-text"
               style={{ color: "black", fontSize: "1.3em" }}
             >
-              <ul style={{ listStyle: "none" }}>
-                <li>
-                  <p style={{ paddingRight: "20px" }}>
-                    Gagan S: <a href="tel:">+91 9008243280</a>
-                  </p>
-                </li>
-
-                <li>
-                  <p>
-                    Tarana Shetty: <a href="tel:">+91 7483408791</a>
-                  </p>
-                </li>
-              </ul>
-            </div>
-            <h2 style={{ fontFamily: "pixeloidsans-bold", color: "black" }}>
-              Connect with Us On
-            </h2>
-            <div
-              style={{
-                display: "flex",
-                alignItems: "center",
-                justifyContent: "center",
-                paddingTop: "5px",
-              }}
-            >
-              <div style={{ margin: "0 10px" }}>
-                <a
-                  style={{ color: "black" }}
-                  href="https://www.instagram.com/wearehackman/"
-                  rel="noreferrer noopener"
-                  target="_blank"
-                >
-                  <Instagram size={50} />
-                </a>
-              </div>
-              <div style={{ margin: "0 10px" }}>
-                <a
-                  style={{ color: "black" }}
-                  href="https://www.linkedin.com/company/wearehackman/"
-                  rel="noreferrer noopener"
-                  target="_blank"
-                >
-                  <Linkedin size={50} />
-                </a>
-              </div>
-              <div style={{ margin: "0 10px" }}>
-                <a
-                  style={{ color: "black" }}
-                  href="mailto:genesis.hackman@gmail.com"
-                  rel="noreferrer noopener"
-                  target="_blank"
-                >
-                  <Mail size={50} />
-                </a>
-              </div>
-            </div>
-            <div style={{ color: "black", paddingTop: "20px" }}>
               <p>
-                © 2024 Hackman. All rights Reserved <br />
-                Made by team{" "}
-                <a
-                  href="https://www.instagram.com/genesis.ise/"
-                  rel="noreferrer noopener"
-                  target="_blank"
-                >
-                  Genesis
-                </a>
+                Gagan S: <a href="tel:">+91 9008243280</a>
               </p>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="madeby text-center font-semibold text-white p-2 bg-[black]">Made by team <a href="https://www.instagram.com/genesis.ise/" rel='noreferrer noopener' target="_blank" className='text-purple-300'>Genesis</a> with ❤️</div>
-    </div>
-  );
-}
-
-export default Footer;
+              <p>
+                Tarana Shetty: <a href="tel:">+91 7483408791</a>
+              </p>
+            </div> */}
