1d1caba49f868e91999aa68ea5a1c2430c7440f3
Astraxx04 <gagan200254@gmail.com>
2024-05-09 16:05:14 +0530

    safari fix

--- frontend/src/components/Landing.jsx
+++ frontend/src/components/Landing.jsx
@@ -8,7 +8,7 @@ function Landing() {
           <h1 className="title" style={{ filter: "drop-shadow(4px 4px 6px #010101)" }}>
             H A C K M A N
           </h1>
-          <h3 className="karma2" >June 8th-9th</h3>
+          <div className="dates" >June 8th-9th</div>
         </div>
     </div>
   );
