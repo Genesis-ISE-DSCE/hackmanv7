3a6802d8137fc85581645f3e31ca67a8da8ce0d5
Aditya <code.adi8103@gmail.com>
2024-05-14 22:10:20 +0530

    Get team details3

--- backend/controllers/Leader.ts
+++ backend/controllers/Leader.ts
@@ -61,11 +61,11 @@ export const login = async (req: Request, res: Response) => {
 
 //GET TEAM DETAILS
 export const getTeamDetails = async (req: Request, res: Response) => {
-  const id = req.user.id as string;
+  const { id } = req.user;
 
   const team = await db.team.findUnique({
     where: {
-      leaderId: id,
+      id,
     },
     include: {
       members: true,
