342db97b299261b76728fc5abeb8749e8db8d38b
Janesh7 <janeshwalia273@gmail.com>
2024-05-16 23:33:42 +0530

    css for admin login

--- frontend/src/components/Admin/AdminLogin.jsx
+++ frontend/src/components/Admin/AdminLogin.jsx
@@ -2,9 +2,6 @@ import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import '../../css/admin.css';
 
-// const realusername = process.env.REACT_APP_USERNAME;
-// const realpassword = process.env.EACT_APP_PASSWORD;
-
 function AdminLogin({ updateIsAuthenticated }) {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
@@ -14,8 +11,6 @@ function AdminLogin({ updateIsAuthenticated }) {
 
   const handleLogin = (event) => {
     event.preventDefault();
-    console.log('Username:', username);
-    console.log('Password:', password);
     if (username === 'sayanur' && password === 'whyareyoulikethis') {
       updateIsAuthenticated(true);
       navigate("/admincontrol");
@@ -25,37 +20,38 @@ function AdminLogin({ updateIsAuthenticated }) {
   };
 
   return (
-    <div id='admin' className='custom-reg-bg textfonts d-flex justify-content-center align-items-start'>
-      <div className='card p-4 rounded-lg bg-gray-800 w-75-lg kard '>
-        <h1 className='text-center font-bold text-3xl' style={{ color: '#fff' }}>
-          Admin
-        </h1>
-        {errorMessage && <div className="alert alert-danger">{errorMessage}</div>}
-        <form className='my-3' onSubmit={handleLogin}>
-          <div className='mb-3'>
-            <input
-              className='form-control'
-              placeholder='Username'
-              type='text'
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-            />
+    <div>
+    <div className='background-scroll'></div>
+      <div className='content'>
+        <div className='card-container'>
+          <div className='card'>
+            <h1>Admin</h1>
+            {errorMessage && <div className="alert">{errorMessage}</div>}
+            <form onSubmit={handleLogin}>
+              <div className='mb-3'>
+                <input
+                  className='form-control'
+                  placeholder='Username'
+                  type='text'
+                  value={username}
+                  onChange={(e) => setUsername(e.target.value)}
+                />
+              </div>
+              <div className='mb-3'>
+                <input
+                  className='form-control'
+                  placeholder='Password'
+                  type='password'
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </div>
+              <button type='submit' className='btn'>
+                Login
+              </button>
+            </form>
           </div>
-          <div className='mb-3'>
-            <input
-              className='form-control'
-              placeholder='Password'
-              type='password'
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-          </div>
-          <div className='d-flex justify-content-between'>
-            <button type='submit' className='btn  w-45'>
-              Login
-            </button>
-          </div>
-        </form>
+        </div>
       </div>
     </div>
   );
