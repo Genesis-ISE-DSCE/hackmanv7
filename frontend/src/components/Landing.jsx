6909e7455ec5d85f913548dc05b9aec9c25adf43
Janesh7 <janeshwalia273@gmail.com>
2024-05-17 00:05:59 +0530

    Minor changes

--- frontend/src/components/Landing.jsx
+++ frontend/src/components/Landing.jsx
@@ -3,7 +3,7 @@ import "../css/landing.css";
 
 function Landing() {
   return (
-    <div id="land" >
+    <div id="land" className="custom-height-land">
         <div className="karma glass">
           <h1 className="title" style={{ filter: "drop-shadow(4px 4px 6px #010101)" }}>
             H A C K M A N
