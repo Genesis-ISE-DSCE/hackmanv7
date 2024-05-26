32829b0b4518e187683ca429a5454e4956802c6b
Astraxx04 <gagan200254@gmail.com>
2024-05-26 11:17:08 +0530

    Registration open for a while

--- frontend/src/pages/RegistrationPage.jsx
+++ frontend/src/pages/RegistrationPage.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import "../App.css";
 import "../css/register.css";
-import OpensSoon from "../components/OpensSoon";
+// import OpensSoon from "../components/OpensSoon";
+import Register from "../components/Register";
 
 const RegistrationPage = () => {
     return (
         <div>
             <div className="background-scroll"></div>
             <div className="content">
-                <OpensSoon />
+                <Register />
             </div>
         </div>
     );
